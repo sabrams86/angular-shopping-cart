d3de51f8cd109bdaa64df2b1e8f7b298253b6c09
Steven Abrams <steven.abrams86@gmail.com>
2015-08-18 20:26:06 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("shoppingCart", ['ngRoute', 'ngAnimate']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home',
+      controller: 'HomeController'
+    })
+    .when('/cart', {
+      templateUrl: 'partials/cart',
+      controller: 'CartController'
+    })
+}]);
